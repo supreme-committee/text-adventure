d84cc692e57b6c940430ac08f0aeed0d2c9b6766
Michael Grijalva <gman_michael@hotmail.com>
2013-11-07 12:39:53 -0800

    Implemented better handling of command line arguments

--- src/Main.cc
+++ src/Main.cc
@@ -17,30 +17,32 @@ int main(int argc, char** argv)
 
 	if (argc > 3)
 	{
-		Logger::log("ERROR: you must specify the game tar on the command line");
-		Logger::log("Usage: game-engine.exe [tar file]");
-		cerr << "Usage: game-engine.exe [tar file]" << endl;
+		cout << "Usage: text-adventure.exe [.tar file] [-l]" << endl;
 		return 1;
 	}
 
-	if (argc == 3 && strcmp(argv[2], "-l") == 0) // If user wants logging turned on
+	Game game;
+	string tarFile; // The tar file the user provided
+	for (int i = 1; i < argc; i++)
 	{
-		cout << "logging turned on" << endl;
-		Logger::init();
+		string arg(argv[i]);
+		if (arg == "-l") Logger::init();
+		else if (arg.substr(arg.length() - 4, arg.length() - 1) == ".tar")
+		{
+			tarFile = argv[i];
+		}
 	}
 
-    Game game;
-	if (argc == 1) // No tar file was given
+	if (tarFile.length() > 0) // Initialize game with given tar file
+	{
+		if (!game.init(tarFile)) return 1;
+	}
+	else // initialze game without starting file
 	{
 		if (!game.init()) return 1;
 	}
-	else if (argc > 1 && !game.init(argv[1])) // Start engine with given tar file
-	{
-		Logger::log("Game initialization failed. See previous messages");
-		return 1;
-	}
 	
-	while (!game.isDone())
+	while (!game.isDone()) // Run the game loop
 	{
 		game.input();
 		game.update();
