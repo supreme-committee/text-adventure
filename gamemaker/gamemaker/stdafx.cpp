448b11a2be882d48253ff5addb8e2fe718a419d5
Michael Parker <msparker@ucsc.edu>
2013-11-22 22:16:57 -0800

    Adding the first version of the game creator
    I ended up using VS 2008 to make this since apparently 2012 doesn't have
    forms in c++ anymore and I'm too lazy to mess around with c#. I tried to
    keep as much of the clutter out of the repository as I could but if it's
    missing something it needs to run, let me know.
    As of now, I've tested all the xml tags it generates except for the
    conditionals and variables.

---
+++ gamemaker/gamemaker/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// gamemaker.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
