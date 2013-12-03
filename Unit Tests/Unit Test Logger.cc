de3b8098092395ea1a250d44d62fec7b21bc8950
blieu <blieu@ucsc.edu>
2013-12-03 11:21:26 -0800

    File that I will write the equivalence class in.

---
+++ Unit Tests/Unit Test Logger.cc
@@ -0,0 +1,3 @@
+Benjamin Lieu
+
+So I will try to write up the equivalence classes for the logger.cc but if you see something wrong with it please tell me because I'm not sure if I am writing an equivalence class that actually applies to this class.
