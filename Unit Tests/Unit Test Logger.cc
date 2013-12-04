3bb28b71a1b1569c9eef21ddfca75541abd8acfd
Michael Grijalva <gman_michael@hotmail.com>
2013-12-04 12:53:45 -0800

    Removed Unit Tests directory (no longer used)

--- Unit Tests/Unit Test Logger.cc
+++
@@ -1,3 +0,0 @@
-Benjamin Lieu
-
-So I will try to write up the equivalence classes for the logger.cc but if you see something wrong with it please tell me because I'm not sure if I am writing an equivalence class that actually applies to this class.
